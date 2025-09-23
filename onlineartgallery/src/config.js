b3e0af9dc60b7af3cd9f916492f13e9f983b5f16
KothakottaSaiLaxmanRao <163739651+2300090230@users.noreply.github.com>
2025-09-23 07:15:41 +0530

    Update API URL in config.js

--- onlineartgallery/src/config.js
+++ onlineartgallery/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    //url: "http://localhost:2030/apionlineart"
-    url: "http://localhost:2069"
+    url: "http://localhost:2030/apionlineart"
+    //url: "http://localhost:2069"
 }
 
 export default config;
